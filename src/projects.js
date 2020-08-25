2e9fa46317b02d6ac8b759325cca0d0c8e621682
Ritik Mody <ritikashishmody@gmail.com>
2020-08-25 18:17:13 +0530

    added project

--- src/projects.js
+++ src/projects.js
@@ -8,6 +8,11 @@ const projects=[
         "title":"Learn2Code",
         "description":"It is an elearning website for different coding languages.",
         "software":["PHP"]
+    },
+    {
+        "title":"Youtube Comments Sentiment Analyser",
+        "description":"It is a sentiment analyser for Youtube comments.",
+        "software":["Vader Algorithm"]
     }
 ];
 
